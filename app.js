e6126bc145827cceebd3ce1a38d24ab949b4e7bf
Lucas Vinicius <lucas.montenegro@somosphi.com>
2020-02-21 17:59:17 -0300

    proxy

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
 const express = require("express");
 const bodyParser = require('body-parser');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 const userRoutes = require("./routers/user");
+const proxyRoutes = require("./routers/proxy");
+const restream = function(proxyReq, req, res, options) {
+  if (req.body) {
+      let bodyData = JSON.stringify(req.body);
+      // incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
+      proxyReq.setHeader('Content-Type','application/json');
+      proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
+      // stream the content
+      proxyReq.write(bodyData);
+  }
+}
+
+const apiProxy = createProxyMiddleware('/proxy', {
+  target: 'http://localhost:6666/user/',
+  onProxyReq: restream,
+  pathRewrite: {
+    '^/proxy': '', // remove path
+  }, 
+});
 
 const autentication = function(req, res, next) {
   console.log("1 autentication");
   next();
 };
-app.use(bodyParser.json());
+
 app.use(autentication);
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use("/proxy", proxyRoutes);
+app.use(apiProxy);                // with restreaming
+// app.use('/proxy', createProxyMiddleware( { target: 'http://localhost:6666/user/', changeOrigin: true, pathRewrite: {
+//   '^/proxy': '', // remove path
+// }, }));
+
 app.use("/user", userRoutes);
 
 const port = 3333;
--- routers/proxy.js
+++ routers/proxy.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const { check, validationResult } = require("express-validator");
 const router = express.Router();
+
 const userValidator = [check("name").isString(), check("email").isEmail()];
+
 const errorValidator = (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -12,19 +14,21 @@ const errorValidator = (req, res) => {
 router
   .route("/")
   .get((req, res, next) => {
-    console.log("/ Get");
-    res.status(200).send("/ GET");
+    console.log("/ Get PROXY");
+    name = req.body.name;
+    console.log(name) 
+    //res.status(200).send("/ GET PROXY");
     next();
   })
-  .post(userValidator, (req, res, next) => {
-    errorValidator(req, res);
-    const name = req.body.name;
-    const email = req.body.email;
-    console.log(name);
-    console.log(email);
-    console.log("/ Post");
-    res.status(200).send(`/ POST ${name}, ${email} `);
-    next();
-  });
+  // .post(userValidator, (req, res, next) => {
+  //   errorValidator(req, res);
+  //   const name = req.body.name;
+  //   const email = req.body.email;
+  //   console.log(name);
+  //   console.log(email);
+  //   console.log("/ Post");
+  //   res.status(200).send(`/ POST ${name}, ${email} `);
+  //   next();
+  // });
 
 module.exports = router;
