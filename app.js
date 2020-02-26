be4573df79aeac4763e64cfb6eb0562f33981eb1
Lucas Vinicius <lucas.montenegro@somosphi.com>
2020-02-26 17:36:37 -0300

    change method on proxyReq.method

--- app.js
+++ app.js
@@ -8,15 +8,13 @@ const app = express();
 
 const restream = function(proxyReq, req, res, options) {
   if (req.body) {
+    proxyReq.method = 'POST';
     let bodyData = JSON.stringify(req.body);
     // incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
     proxyReq.setHeader("Content-Type", "application/json");
     proxyReq.setHeader("Content-Length", Buffer.byteLength(bodyData));
     // stream the content
     proxyReq.write(bodyData);
-  }
-  if(res.send){
-    console.log(res)
   }
 };
 
