d7d38109f7fecaeb1d8c0e8f12a186ba6a2ef16a
Lucas Vinicius <lucas.montenegro@somosphi.com>
2020-02-26 17:06:37 -0300

    edit

--- app.js
+++ app.js
@@ -1,41 +1,44 @@
 const express = require("express");
-const bodyParser = require('body-parser');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const app = express();
+const bodyParser = require("body-parser");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 const userRoutes = require("./routers/user");
 const proxyRoutes = require("./routers/proxy");
+
+const app = express();
+
 const restream = function(proxyReq, req, res, options) {
   if (req.body) {
-      let bodyData = JSON.stringify(req.body);
-      // incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
-      proxyReq.setHeader('Content-Type','application/json');
-      proxyReq.setHeader('Content-Length', Buffer.byteLength(bodyData));
-      // stream the content
-      proxyReq.write(bodyData);
+    let bodyData = JSON.stringify(req.body);
+    // incase if content-type is application/x-www-form-urlencoded -> we need to change to application/json
+    proxyReq.setHeader("Content-Type", "application/json");
+    proxyReq.setHeader("Content-Length", Buffer.byteLength(bodyData));
+    // stream the content
+    proxyReq.write(bodyData);
   }
-}
+  if(res.send){
+    console.log(res)
+  }
+};
 
-const apiProxy = createProxyMiddleware('/proxy', {
-  target: 'http://localhost:6666/user/',
+const apiProxy = createProxyMiddleware("/proxy", {
+  target: "http://localhost:6666/proxy2",
   onProxyReq: restream,
   pathRewrite: {
-    '^/proxy': '', // remove path
-  }, 
+    "^/proxy": "" // remove path
+  }
 });
 
 const autentication = function(req, res, next) {
-  console.log("1 autentication");
+  console.log("autentication middleware");
   next();
 };
 
 app.use(autentication);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
+
 app.use("/proxy", proxyRoutes);
 app.use(apiProxy);                // with restreaming
-// app.use('/proxy', createProxyMiddleware( { target: 'http://localhost:6666/user/', changeOrigin: true, pathRewrite: {
-//   '^/proxy': '', // remove path
-// }, }));
 
 app.use("/user", userRoutes);
 
--- routers/proxy.js
+++ routers/proxy.js
@@ -2,33 +2,26 @@ const express = require("express");
 const { check, validationResult } = require("express-validator");
 const router = express.Router();
 
-const userValidator = [check("name").isString(), check("email").isEmail()];
+const proxyValidator = [check("name").isString()];
 
-const errorValidator = (req, res) => {
+const errorValidator = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(422).json({ errors: errors.array() });
   }
+  next();
 };
 
 router
   .route("/")
-  .get((req, res, next) => {
+  .get(proxyValidator, errorValidator, (req, res, next) => {
     console.log("/ Get PROXY");
     name = req.body.name;
-    console.log(name) 
+    console.log(name)
+    req.body.name += ' sobrenome' 
     //res.status(200).send("/ GET PROXY");
     next();
   })
-  // .post(userValidator, (req, res, next) => {
-  //   errorValidator(req, res);
-  //   const name = req.body.name;
-  //   const email = req.body.email;
-  //   console.log(name);
-  //   console.log(email);
-  //   console.log("/ Post");
-  //   res.status(200).send(`/ POST ${name}, ${email} `);
-  //   next();
-  // });
+
 
 module.exports = router;
